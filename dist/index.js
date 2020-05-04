b967b5479ff71a079e8871bb6728b409f24be41b
xariveros <xariveros@uc.cl>
2020-05-03 22:24:31 -0400

    run build

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,8 @@ function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
+var PORT = process.env.PORT || 3000;
+
 function main() {
   return _main.apply(this, arguments);
 }
@@ -19,7 +21,7 @@ function _main() {
         switch (_context.prev = _context.next) {
           case 0:
             _context.next = 2;
-            return _app["default"].listen(3000);
+            return _app["default"].listen(PORT);
 
           case 2:
             console.log("server en 3000");
